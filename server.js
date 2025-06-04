8bed9e13892cfc4eb5b08ab8bde900336e0a033d
koshtijatin26 <koshtijatin26@gmail.com>
2025-06-04 22:01:09 +0530

    general

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ dotenv.config({});
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
