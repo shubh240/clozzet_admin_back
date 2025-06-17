b68da5fe07293e26d1295ee3ff75cc685ad14aad
koshtijatin26 <koshtijatin26@gmail.com>
2025-06-18 00:10:45 +0530

    general

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/logout",isSuperAdminAuthenticated, logout);
+router.get("/logout",logout);
 
 console.log("Admin route")
 
