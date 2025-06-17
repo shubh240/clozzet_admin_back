3af56a71d335d1d91c54a26027194e24eed3414a
koshtijatin26 <koshtijatin26@gmail.com>
2025-06-18 00:17:44 +0530

    logout error

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/logout",logout);
+router.get("/logout",isSuperAdminAuthenticated, logout);
 
 console.log("Admin route")
 
